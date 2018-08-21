ab6b2523149b576c12cf443bc1b83176f68fbd2a
DonCorleone <vitocorleone77@gmail.com>
2018-08-21 22:33:42 +0200

    staic files handling in client, IHostingEnvironment

--- Classes/ImageWriter.cs
+++ Classes/ImageWriter.cs
@@ -9,61 +9,61 @@ using Microsoft.AspNetCore.Http;
 
 namespace ImageWriter.Classes
 {
-   public class ImageWriter : IImageWriter
-   {
-      public async Task<string> UploadImage(IFormFile file)
+  public class ImageWriter : IImageWriter
+  {
+    public async Task<string> UploadImage(IFormFile file, string directory)
+    {
+      if (CheckIfImageFile(file))
       {
-         if (CheckIfImageFile(file))
-         {
-            return await WriteFile(file);
-         }
-
-         return "Invalid image file";
+        return await WriteFile(file, directory);
       }
 
-      /// <summary>
-      /// Method to check if file is image file
-      /// </summary>
-      /// <param name="file"></param>
-      /// <returns></returns>
-      private bool CheckIfImageFile(IFormFile file)
-      {
-         byte[] fileBytes;
-         using(var ms = new MemoryStream())
-         {
-            file.CopyTo(ms);
-            fileBytes = ms.ToArray();
-         }
+      return "Invalid image file";
+    }
 
-         return WriterHelper.GetImageFormat(fileBytes) != WriterHelper.ImageFormat.unknown;
+    /// <summary>
+    /// Method to check if file is image file
+    /// </summary>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    private bool CheckIfImageFile(IFormFile file)
+    {
+      byte[] fileBytes;
+      using(var ms = new MemoryStream())
+      {
+        file.CopyTo(ms);
+        fileBytes = ms.ToArray();
       }
 
-      /// <summary>
-      /// Method to write file onto the disk
-      /// </summary>
-      /// <param name="file"></param>
-      /// <returns></returns>
-      public async Task<string> WriteFile(IFormFile file)
+      return WriterHelper.GetImageFormat(fileBytes) != WriterHelper.ImageFormat.unknown;
+    }
+
+    /// <summary>
+    /// Method to write file onto the disk
+    /// </summary>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    public async Task<string> WriteFile(IFormFile file, string directory)
+    {
+      string fileName;
+      try
       {
-         string fileName;
-         try
-         {
-            var extension = "." + file.FileName.Split('.') [file.FileName.Split('.').Length - 1];
-            fileName = Guid.NewGuid().ToString() + extension; //Create a new Name 
-            //for the file due to security reasons.
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+        var extension = "." + file.FileName.Split('.') [file.FileName.Split('.').Length - 1];
+        fileName = Guid.NewGuid().ToString() + extension; //Create a new Name 
+        //for the file due to security reasons.
+        var path = Path.Combine(directory, fileName);
 
-            using(var bits = new FileStream(path, FileMode.Create))
-            {
-               await file.CopyToAsync(bits);
-            }
-         }
-         catch (Exception e)
-         {
-            return e.Message;
-         }
-
-         return fileName;
+        using(var bits = new FileStream(path, FileMode.Create))
+        {
+          await file.CopyToAsync(bits);
+        }
       }
-   }
+      catch (Exception e)
+      {
+        return e.Message;
+      }
+
+      return fileName;
+    }
+  }
 }
